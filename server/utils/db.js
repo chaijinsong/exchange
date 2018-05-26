1dee37d37092568e8397f53ea1b537c8e8365f47
cjs <756423140@qq.com>
2018-05-26 11:29:33 +0800

    修改数据库ip地址

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,9 +1,9 @@
 //连接数据库的js，导出连接对象
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
+  host     : '47.94.91.226',
   user     : 'root',
-  password : 'root',
+  password : '',
   database : 'exchange'
 });
  
