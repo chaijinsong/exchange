dfb4d61839bcadd8489babcbca6653f04b8e1fb4
cjs <756423140@qq.com>
2018-05-26 15:28:20 +0800

    将后台的api从localhost修改成服务器ip并且修改对应端口

--- server/utils/api.js
+++ server/utils/api.js
@@ -1,6 +1,6 @@
 const API1 = '/api/v1';
 const API2 = '/api/v2';
 
-// let api = 'http://172.16.142.168:3001';
-let api = 'http://localhost:3001'
+let api = 'http://47.94.91.226:3389';
+// let api = 'http://localhost:3001'
 module.exports = api;
