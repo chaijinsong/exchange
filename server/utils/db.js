ceb88da99437896c01a49abcd298fa8dab0ee17c
cjs <756423140@qq.com>
2018-05-29 16:40:56 +0800

    banner获取数据不对，修改接口

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,9 +1,9 @@
 //连接数据库的js，导出连接对象
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : '47.94.91.226',
+  host     : 'localhost',
   user     : 'root',
-  password : '',
+  password : 'root',
   database : 'exchange'
 });
  
