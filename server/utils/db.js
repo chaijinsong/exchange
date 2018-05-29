1815d1b61f075c61c249445b18e5eff9207a8a3e
cjs <756423140@qq.com>
2018-05-29 16:46:27 +0800

    修改端口

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,9 +1,9 @@
 //连接数据库的js，导出连接对象
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
+  host     : '47.94.91.226:3389',
   user     : 'root',
-  password : 'root',
+  password : '',
   database : 'exchange'
 });
  
