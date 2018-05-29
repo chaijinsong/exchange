e334b2dc3d4ad7377fcaf3b9f50ee26d95b2adbd
cjs <756423140@qq.com>
2018-05-29 20:26:49 +0800

    修改后台

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,12 +1,18 @@
 //连接数据库的js，导出连接对象
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : '47.94.91.226:3389',
+  host     : '47.94.91.226',
   user     : 'root',
   password : '',
   database : 'exchange'
 });
- 
+
+// var connection = mysql.createConnection({
+//     host:'localhost',
+//     user:'root',
+//     password:'root',
+//     database:'exchange'
+// })
 connection.connect();
 
 module.exports = connection;
